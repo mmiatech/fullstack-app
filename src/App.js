ec1a0a07af866ee3ff97b6548a6e3d70de22634a
mmiatech <mmiatech@madisoncollege.edu>
2020-12-13 14:44:45 -0600

    imports, index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <h1>
+          Hello World!
+        </h1>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
